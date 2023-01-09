025386a5ca5bbd72963bbe1146abf5cb6c46912a
ChrisIuil <okoriec01@gmail.com>
2023-01-09 12:30:47 +0100

    added model to the news prt

--- lib/news_bloc.dart
+++ lib/news_bloc.dart
@@ -1,13 +1,37 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:bloc_projet/model.dart';
+import 'package:http/http.dart' as http;
 
 enum NewsAction { fetch }
 
 class NewsBloc {
-  final _newsStreamController = StreamController<List>();
-  StreamSink<List> get newsSink => _newsStreamController.sink;
-  Stream<List> get newsStream => _newsStreamController.stream;
+  final _newsStreamController = StreamController<List<ArticleElement>>();
+  StreamSink<List<ArticleElement>> get newsSink => _newsStreamController.sink;
+  Stream<List<ArticleElement>> get newsStream => _newsStreamController.stream;
 
   final _newsActionController = StreamController<NewsAction>();
   StreamSink<NewsAction> get newsActionSink => _newsActionController.sink;
   Stream<NewsAction> get newsActionStream => _newsActionController.stream;
+  NewsBloc() {
+    newsActionStream.listen((event) async {
+      if (event == NewsAction.fetch) {
+        var newsData = await getNews();
+      }
+    });
+  }
+  Future<Article> getNews() async {
+    var newsModel;
+    var url = Uri.parse(
+        'https://newsapi.org/v2/everything?q=nigeria&sortBy=popularity&apiKey=62c7e0784d884e639ab0125442ece977');
+    try {
+      var getApi = await http.get(url);
+      if (getApi.statusCode == 200) {
+        final data = getApi.body;
+        var jsondata = json.decode(data);
+        newsModel = Article.fromJson(jsondata);
+      }
+    } catch (e) {}
+    return newsModel;
+  }
 }
