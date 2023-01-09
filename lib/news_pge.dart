42287e659f0a4c9d6089e54d0a10a2c0a534910f
ChrisIuil <okoriec01@gmail.com>
2023-01-09 13:32:35 +0100

    done with basic next step now

--- lib/news_pge.dart
+++ lib/news_pge.dart
@@ -1,8 +1,10 @@
 import 'package:bloc_projet/model.dart';
 import 'package:bloc_projet/news_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -23,16 +25,45 @@ class _NewsPageState extends State<NewsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('NewsPage')),
-      body: StreamBuilder<List<ArticleElement>>(
-          stream: newsBloc.newsStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text('data');
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return const Center(child: CircularProgressIndicator());
-          }),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 900,
+              child: StreamBuilder<List<ArticleElement>>(
+                  stream: newsBloc.newsStream,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            var article = snapshot.data![index];
+                            var formattedTime = DateFormat('dd MMM -HH:mm')
+                                .format(article.publishedAt!);
+                            return ListTile(
+                              title: Text(article.title.toString()),
+                              leading: Container(
+                                width: 200,
+                                height: 150,
+                                child: CachedNetworkImage(
+                                  imageUrl: article.urlToImage!,
+                                  placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                              ),
+                            );
+                          });
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}' ?? 'error');
+                    }
+                    return const Center(child: CircularProgressIndicator());
+                  }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
